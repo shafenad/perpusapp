25250c17538be89e65b480e4c3f4e26797b38027
shafenad <73510961+shafenad@users.noreply.github.com>
2023-09-30 16:52:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using perpus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat objek buku
+        Buku book1 = new Buku("Kata", "Rintik sedu");
+
+
+        // Membuat objek anggota perpustakaan
+        Member member1 = new Member("Farhan");
+
+
+        // Membuat objek peminjaman
+        //peminjaman pinjam = new peminjaman(book1, member1);
+
+        // Membuat objek peminjaman dengan tanggal kembali
+        DateTime tanggalKembali = DateTime.Now.AddDays(20); // Contoh tanggal pengembalian, misalnya 14 hari dari sekarang
+        peminjaman pinjam = new peminjaman(book1, member1, tanggalKembali);
+
+        // Menampilkan informasi peminjaman dengan tanggal kembali
+        pinjam.tampilinfo();
+
+
+        // Menampilkan informasi buku
+        book1.tampilinfo();
+
+        // Menampilkan informasi peminjaman
+        //pinjam.tampilinfo();
+
+
+        // Menampilkan judul buku dan nama anggota yang meminjam dari informasi peminjaman
+        Console.WriteLine("_________________________________________________");
+        Console.WriteLine("INFORMASI PEMINJAMAN BUKU PERPUSTAKAAN");
+        Console.WriteLine("_________________________________________________");
+        Console.WriteLine($"\nBuku yang dipinjam: {pinjam.GetJudulBuku()}");
+        Console.WriteLine($"Anggota yang meminjam: {pinjam.GetNamaAnggota()}");
+        Console.WriteLine($"Tanggal pinjam: {pinjam.Gettanggalpinjam():yyyy-MM-dd}");
+        Console.WriteLine($"Tanggal kembali: {pinjam.Gettanggalkembali():yyyy-MM-dd}");
+
+
+
+    }
+}
---
+++ perpus.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace perpus
+{
+    // Interface untuk item perpustakaan
+    public interface Iitem_perpus
+    {
+        void tampilinfo();
+    }
+
+    // Kelas abstrak untuk item perpustakaan
+    public abstract class item_perpus : Iitem_perpus
+    {
+        private string judul;
+
+        public item_perpus(string _judul)
+        {
+            this.judul = _judul;
+        }
+
+        public string Getjudul()
+        {
+            return judul;
+        }
+
+        public abstract void tampilinfo();
+    }
+
+    // Kelas untuk buku
+    public class Buku : item_perpus
+    {
+        private string pengarang;
+
+
+        public Buku(string _judul, string _pengarang) : base(_judul)
+        {
+            this.pengarang = _pengarang;
+        }
+
+        public string Getnamapengarang()
+        {
+            return pengarang;
+        }
+
+        public override void tampilinfo()
+        {
+            Console.WriteLine($"Judul Buku: {Getjudul()}");
+            Console.WriteLine($"Pengarang: {Getnamapengarang()}");
+        }
+    }
+
+    // Kelas untuk anggota perpustakaan
+    public class Member
+    {
+        private string nama_member;
+
+        public Member(string _nama_member)
+        {
+            this.nama_member = _nama_member;
+        }
+
+        public string Getnamamember()
+        {
+            return nama_member;
+        }
+
+        public void pinjambarang(item_perpus item)
+        {
+            Console.WriteLine($"{Getnamamember()} meminjam '{item.Getjudul()}'");
+        }
+    }
+
+
+    // Kelas untuk peminjaman buku
+    public class peminjaman
+    {
+        private item_perpus item;
+        private Member member;
+        private DateTime tgl_pinjam;
+        private DateTime tgl_kembali; 
+
+        public peminjaman(item_perpus _item, Member _member)
+        {
+            this.item = _item;
+            this.member = _member;
+            this.tgl_pinjam = DateTime.Now;
+        }
+
+        //  overload tanggal kembali
+        public peminjaman(item_perpus _item, Member _member, DateTime _tgl_kembali)
+        {
+            this.item = _item;
+            this.member = _member;
+            this.tgl_pinjam = DateTime.Now;
+            this.tgl_kembali = _tgl_kembali; 
+        }
+
+        public DateTime Gettanggalpinjam()
+        {
+            return tgl_pinjam;
+        }
+
+        public DateTime Gettanggalkembali()
+        {
+            return tgl_kembali;
+        }
+
+        public string GetJudulBuku()
+        {
+            return item.Getjudul();
+        }
+
+        public string GetNamaAnggota()
+        {
+            return member.Getnamamember();
+        }
+
+        public void tampilinfo()
+        {
+            Console.WriteLine($"Tanggal pinjam: {Gettanggalpinjam():yyyy-MM-dd}");
+            Console.WriteLine($"Peminjam: {member.Getnamamember()}");
+            //Console.WriteLine($"Judul buku: {item.Getjudul()}");
+            Console.WriteLine($"Tanggal kembali: {Gettanggalkembali():yyyy-MM-dd}");
+
+        }
+    }
+
+
+}
